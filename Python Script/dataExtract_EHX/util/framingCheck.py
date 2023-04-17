049b4afcc3d006a4ef3518a3c3ba1f9b2ee77864
Jacob OBrien <jobrien@thinkbrave.com>
2023-04-17 16:02:15 -0400

    Added Stud Stop and Hammer logic

--- Python Script/dataExtract_EHX/util/framingCheck.py
+++ Python Script/dataExtract_EHX/util/framingCheck.py
@@ -23,6 +23,7 @@ class Clear:
         results = pgDB.query(sqlStatement=sql_select_query)
         dbc.printResult(results)
         #assign results of query to variables
+        #thickness is in the X direction of elevation, width/length in the Y direction
         self.ss_thickness = float(results[0][1])
         self.ss_width = float(results[1][1])
         self.hu_thickness = float(results[2][1])
@@ -30,19 +31,25 @@ class Clear:
         self.hu_stroke = float(results[4][1])
         pgDB.close()
         
-    def studStopFS(elementPOS):
+    def studStopFS(elementPOS, current_stud, self):
+        #elementPOS and current_stud are lists = E1X,E1Y,E2X,E2Y,E3X,E3Y,E4X,E4Y
+        #current stud has the X value that we are at, and we use it to check the ss boundries (E4X)
+        if elementPOS[1] >= (self.ss_width + 1.5) and current_stud[6] <= elementPOS[0] and elementPOS[6] <= (current_stud[0] - self.ss_thickness):
+            return False
+        else:
+            return True
 
-        return False
-
-    def hammerFS(elementPOS):
-        
-        return False
+    def hammerFS(elementPOS, current_stud, self):
+        if elementPOS[1] >= (self.hu_length + 1.5) and (current_stud[6] + self.hu_thickness) <= elementPOS[0] and elementPOS[6] <= current_stud[0]:
+            return False
+        else:
+            return True
     
-    def studStopMS(elementPOS):
+    def studStopMS(elementPOS, self):
 
         return False
 
-    def hammerMS(elementPOS):
+    def hammerMS(elementPOS, self):
         
         return False
 
