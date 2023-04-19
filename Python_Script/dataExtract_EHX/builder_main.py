0b629a9616a91accee32440d4babae2f5d628b90
amurray <amurray@thinkbrave.com>
2023-04-19 09:45:53 -0400

    EC1 Structure and Main Program.

---
+++ Python_Script/dataExtract_EHX/builder_main.py
@@ -0,0 +1,35 @@
+import sys
+from util import dataBaseConnect as dbc
+from util import panelData as pd
+from util import EC1
+
+#testing jobID: 221415WALLS
+print(sys.argv[1])
+credentials = dbc.getCred()
+pgDB = dbc.DB_Connect(credentials)
+pgDB.open()
+
+sql_Var = sys.argv[1] # "221415WALLS"
+sql_select_query=f"""SELECT jobid, panelguid
+                    FROM bundle, panel
+                    WHERE jobid = '{sql_Var}'
+                """
+
+results = pgDB.query(sqlStatement=sql_select_query)
+dbc.printResult(results)
+pgDB.close()
+
+# Itterate through each panel to build table data
+for panel in results:
+    # Get Panel Data
+    pdData = pd.Panel(panel)
+    ## Stud Picker Data
+    
+    ## Framing
+    fm3_panel = EC1.JobData(pdData)
+    #fm3_panel.jobdata.append(linelist)
+    ## Robot Cell 1
+
+    ## Robot Cell 2
+
+
