25b623027dd1306e0073ffe4bba58fcf77858762
Jacob OBrien <jobrien@thinkbrave.com>
2023-04-20 15:43:52 -0400

    Update temp.py

--- Python_Script/dataExtract_EHX/util/temp.py
+++ Python_Script/dataExtract_EHX/util/temp.py
@@ -1,8 +1,8 @@
 #tmp = 'panelguid, XPos, OpText, OpCode_FS, ZPos_FS, YPos_FS, SsUpPos_FS, OpCode_MS, ZPos_MS, YPos_MS, SsUpPos_MS, ImgName, OBJ_ID, loadDate'
 #print(tmp.lower())
 
-OpList = [[0,'ikl',69,5,0,0,69,6,0,0,'img',0],[0,'ikl',69,0,0,0,69,0,0,0,'img',0],
-          [1,'ikl',69,0,0,0,69,6,0,0,'img',0],[1,'ikl',69,0,0,0,69,1,0,0,'img',0]]
+OpList = [[1,'ikl',69,0,0,0,69,6,0,0,'img',0],[1,'ikl',69,0,0,0,69,1,0,0,'img',0],
+          [0,'ikl',69,5,0,0,69,6,0,0,'img',0],[0,'ikl',69,0,0,0,69,0,0,0,'img',0]]
 
 count = 1
 
