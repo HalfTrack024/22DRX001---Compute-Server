2bf862c52761493dad85f936e50e5dd9fdee17a9
amurray <amurray@thinkbrave.com>
2023-04-26 15:31:45 -0400

    Built Parameter Fetcher

--- Python_Script/dataExtract_EHX/util/EC2_3.py
+++ Python_Script/dataExtract_EHX/util/EC2_3.py
@@ -1,47 +1,25 @@
 import dataBaseConnect as dbc
 import framingCheck as fc
 import panelData
-
-class missionData_RBC:
-
-    missionID : int
-    info_01 : float
-    info_02 : float
-    info_03 : float
-    info_04 : float
-    info_05 : float
-    info_06 : float
-    info_07 : float
-    info_08 : float 
-    info_09 : float
-    info_10 : int
-    info_11 : int
-
-class BoardData_RBC:
-    board : missionData_RBC
-    boardPlace : missionData_RBC 
-    Fastening : list[missionData_RBC]   
-
-class BoardData_RBC:
-    board : list[BoardData_RBC]
-    missions : list[missionData_RBC]
-
-
-    def __init__(self) -> None:
-        pass
-
-class Layer:
-
+import runData_Helper as rdh
 
 
 class RunData:
 
-    Miss
+    def __init__(self, panel : panelData.Panel):
+        self.panel = panel
 
-    def __init__(self):
+        dbc.credentials()
+        pgDB = dbc.DB_Connect()
+        pgDB.open()
+        
+
+        
+
+    def rdEC2_Main(self):
         pass
 
-    def rdMain(self):
+    def rdEC3_Main(self):
         pass
 
     def getPanel(self):
@@ -52,3 +30,6 @@ class RunData:
 
     def tempFasten(self):
         pass
+
+
+
---
+++ Python_Script/dataExtract_EHX/util/Parameters.py
@@ -0,0 +1,60 @@
+import dataBaseConnect as dbc
+
+
+
+class Parameters: #This class is designed to fetch all parameters from parameter table based on sectionName 
+
+    parmList = []
+
+    def __init__(self, tabs):
+        self.tabNames = tabs
+        credentials = dbc.getCred()
+        pgDB = dbc.DB_Connect(credentials)
+        pgDB.open()
+        for tab in self.tabNames:
+            sql_var = tab
+            sql_select_query=f"""
+                                select 
+                                    json_object_agg(description, jsonOBJ.parms) as varm
+                                from (
+                                        select	
+                                            description,
+                                            json_build_object(
+                                                            'value', value, 
+                                                            'max', "max", 
+                                                            'min', "min", 
+                                                            'datatype', "DataType")	as parms
+                                        from ec1_parameters
+                                        where sectionname = '{sql_var}') jsonOBJ;
+                                """
+            results = pgDB.query(sqlStatement=sql_select_query)
+            self.parmList.append(results[0][0])
+        pgDB.close()
+    
+    def getParm(self,secName, description): #This function is used for retrieving variable name from parameter information
+        index = self.tabNames.index(secName)
+        dtype = self.parmList[index][description]["datatype"]
+        dVar = self.parmList[index][description]["value"]
+        match dtype:
+            case "STRING":
+                return str(dVar)
+            case "BOOL":
+                return bool(dVar)
+            case "INT":
+                return int(dVar)
+            case "DOUBLE":
+                return float(dVar)
+            case _:
+                print("Not a valid datatype")
+                return None
+
+
+if __name__ == "__main__":
+    tabNames = ["Stud Inverter speeds","Nail Tool MS","Axis LBH","Axis HSP","Computer Settings",
+            "Joint Rules","Studfeeder","Axis Width","Axis GUM","Axis GUF","Axis NPM","Positions",
+            "Nail Tool FS","Stud stack positions","Axis NPF","Axis WAN","Axis SPR",
+            "Program Settings And Parameters","Device Offsets","Program Settings and Parameters"]
+    parms = Parameters(tabNames)
+    val = parms.getParm("Positions", "Stud Stop width")
+    print(type(val))
+    print(val)
