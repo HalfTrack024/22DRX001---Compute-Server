d80298adccb7c6c5dc553902306cf1797dec7564
amurray <amurray@thinkbrave.com>
2023-06-13 15:41:10 -0400

    machineData

--- Python_Script/dataExtract_EHX/util/Parameters.py
+++ Python_Script/dataExtract_EHX/util/Parameters.py
@@ -4,45 +4,52 @@ import dataBaseConnect as dbc
 
 class Parameters(): #This class is designed to fetch all parameters from parameter table based on sectionName 
 
-    _parmList = []
+    
 
-    def __init__(self, tabs):
+    def __init__(self, tabs: list, minID, maxID):
         self._tabNames = tabs
         credentials = dbc.getCred()
         pgDB = dbc.DB_Connect(credentials)
         pgDB.open()
-        for tab in self._tabNames:
-            sql_var = tab
-            sql_select_query=f"""
+        sql_var1 = minID
+        sql_var2 = maxID
+        sql_select_query=f"""                            
+                            select json_object_agg(struct.psect, struct.varm) as larm
+                            from (
                                 select 
+                                    jsonOBJ.sect psect, 
                                     json_object_agg(description, jsonOBJ.parms) as varm
                                 from (
-                                        select	
-                                            description,
-                                            json_build_object(
-                                                            'value', value, 
-                                                            'max', "max", 
-                                                            'min', "min", 
-                                                            'datatype', "DataType")	as parms
-                                        from parameters
-                                        where sectionname = '{sql_var}') jsonOBJ;
-                                """
-            results = pgDB.query(sqlStatement=sql_select_query)
-            self._parmList.append(results[0][0])
+                                    select	
+                                        sectionname sect,
+                                        description,
+                                        json_build_object(
+                                                        'value', value, 
+                                                        'max', "max", 
+                                                        'min', "min", 
+                                                        'datatype', "datatype")	as parms
+                                    from parameters
+                                    where stationid >= '{sql_var1}' and stationid <= '{sql_var2}' ) jsonOBJ
+                                group by jsonOBJ.sect
+                            ) struct;
+                            """
+        results = pgDB.query(sqlStatement=sql_select_query)
+        self._parmList : dict = results[0][0]
         pgDB.close()
     
     def getParm(self,secName, description): #This function is used for retrieving variable name from parameter information
-        index = self._tabNames.index(secName)
-        dtype = self._parmList[index][description]["datatype"]
-        dVar = self._parmList[index][description]["value"]
+        dtype = self._parmList.get(secName)[description]["datatype"]
+        dVar = self._parmList.get(secName)[description]["value"]
         match dtype:
-            case "STRING":
+            case "string":
                 return str(dVar)
-            case "BOOL":
+            case "bool":
                 return bool(dVar)
-            case "INT":
+            case "int":
                 return int(dVar)
-            case "DOUBLE":
+            case "double":
+                return float(dVar)
+            case "lreal":
                 return float(dVar)
             case _:
                 print("Not a valid datatype")
@@ -52,7 +59,7 @@ class Parameters(): #This class is designed to fetch all parameters from paramet
 if __name__ == "__main__":
     tabNames = ["Application"]
     
-    parms = Parameters(tabNames)
+    parms = Parameters(tabNames, 10, 19)
     val = parms.getParm("Application", "Run Level 20 missions (True/false)")
     print(type(val))
     print(val)
