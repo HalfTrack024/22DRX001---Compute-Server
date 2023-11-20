db55690ede669b4e89c555a2701fff1e0ecec463
amurray <amurray@thinkbrave.com>
2023-11-19 20:59:03 -0500

    completed Framer and Material Checks

--- Python_Script/thinkCore/Validator/framerCheck.py
+++ Python_Script/thinkCore/Validator/framerCheck.py
@@ -5,7 +5,7 @@ import logging
 
 def check_op_data(items: list):
     status = 0
-
+    count = 0
     sort_by_objID = sorted(items, key=lambda x: x[12])
 
     if sort_by_objID != items:
@@ -18,16 +18,31 @@ def check_op_data(items: list):
         logging.warning(f"Failed Validation Framer- Order by xpos is not sorted")
         status += 1
 
-    for item in items[:1]:
-        if "Operator" in item[2] and ".png" not in item[11]:
+    for item in items[1:]:
+        if "OperatorConfirmation" in item[2] and item[11] == '':
             logging.warning(f"Failed Validation Framer - Image Required at xpos {item[1]}")
             status += 1
-        if ".png" in item[11] and "Operator" not in item[2]:
+        if item[11] != '' and "OperatorConfirmation" not in item[2]:
             logging.warning(f"Failed Validation Framer - Operator Confirmation Required at xpos {item[1]}")
             status += 1
+        if "Auto" in item[2] and "Nailing" not in item[2]:
+            count += 1
 
     # Return Results
     if status > 0:
-        return False
+        return False, count
     else:
-        return True
+        return True, count
+
+
+def check_stud_feeder(mtrl_data, count):
+    status = 0
+    if mtrl_data[0] != count:
+        status += 1
+        logging.warning(f"Failed Validation StudFeeder - Auto Stud Count Incorrect")
+
+    # Return Results
+    if status > 0:
+        return False, count
+    else:
+        return True, count
